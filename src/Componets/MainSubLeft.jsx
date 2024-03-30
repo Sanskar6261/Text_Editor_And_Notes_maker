e46a557db0f5284a64e81e6a6a8bb832b91402f8
sanskar sharma <sanskarsharma6261@gmail.com>
2024-03-31 02:33:57 +0530

    download button is added

--- src/Componets/MainSubLeft.jsx
+++ src/Componets/MainSubLeft.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import '../Css/MainSubLeft.css'
-const mainSubleft = ({notes,deletenote,activeNoteL,setActiveNote}) => {
+import { json } from 'react-router-dom'
+
+
+const mainSubleft = ({ notes, deletenote, activeNoteL, setActiveNote }) => {
+
+
   return (
-    <div className='notesList'> 
+    <div className='notesList'>
       <input className='SearchField' type="text" placeholder='Search Notes' />
       {
-        notes.map((note)=>(
-              <div key={note.id}  className={`app-sidebar-notes ${note.id===activeNoteL && "active"}`} onClick={()=>setActiveNote(note.id)} >
-              <div className={"app-sidebar-note "}>
-                <div className='sidebar-note-title'>
-                  <strong>{note.title && note.title.substr(0,15)+"..."}</strong>
-                  <button className='deleteBtn' onClick={() => {deletenote(note.id)}}>Delete</button>
-                  <button className='downloadBtn'>Download</button>           
-                </div>
-      
-                <p>{note.body && note.body.substr(0,15)+"..."}</p>
-      
-               <small>date-time : {note.lastdateM}</small>
+        notes.map((note) => (
+          <div key={note.id} className={`app-sidebar-notes ${note.id === activeNoteL && "active"}`} onClick={() => setActiveNote(note.id)} >
+            <div className={"app-sidebar-note "}>
+              <div className='sidebar-note-title'>
+                <strong>{note.title && note.title.substr(0, 15) + "..."}</strong>
+                <button className='deleteBtn' onClick={() => { deletenote(note.id) }}>Delete</button>
+                <button className='downloadBtn'
+                  onClick={() => {
+                    const jsondata=`data:text/json;chatset=utf-8,${encodeURIComponent(JSON.stringify(note.body))}`
+                    const link=document.createElement('a');
+                    link.href=jsondata
+                    link.download=`${note.title}.txt`;
+                    link.click();
+                  }}
+                >Download</button>
               </div>
+
+              <p>{note.body && note.body.substr(0, 15) + "..."}</p>
+
+              <small>date-time : {note.lastdateM}</small>
             </div>
+          </div>
         ))
       }
 
